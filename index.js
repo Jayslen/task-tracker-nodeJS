0a556c0c8f3fe2f897571fb24d1a4360ff244cc1
jayslen <jayslen134@gmail.com>
2024-12-18 22:21:25 -0400

    add: list tasks feture added

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
 import { TasksList } from './tasks-class.js'
-import { readFile } from 'node:fs/promises'
+import { readFile, writeFile } from 'node:fs/promises'
 
-const defaultTasks = []
+const defaultValues = { defaultTasks: [], defaultId: 0 }
+const [action, arg1, arg2] = process.argv.slice(2)
 
 try {
-  const savedTasks = await readFile('tas.json', { encoding: 'utf-8' })
-  defaultTasks.push(...JSON.parse(savedTasks))
-} catch {
-  console.log('initializing file to save tasks')
+  const savedTasks = await readFile('task.json', { encoding: 'utf-8' })
+  const maxId = Math.max(...JSON.parse(savedTasks).map(value => value.id))
+  defaultValues.defaultTasks.push(...JSON.parse(savedTasks))
+  defaultValues.defaultId = maxId
+} catch (e) {
+  if (e.code === 'ENOENT') {
+    writeFile('task.json', JSON.stringify([]))
+  } else {
+    console.error('Something happend')
+    process.exit(1)
+  }
 }
 
-const tasks = new TasksList(defaultTasks)
-console.log(tasks.tasks)
+const tasks = new TasksList(defaultValues.defaultTasks)
+
+switch (action) {
+  case 'list':
+    tasks.list({ status: arg1 })
+    break
+
+  case undefined:
+    console.log('Not command was introduced')
+    break
+  default:
+    console.log(`Actions '${arg1} does not exit'`)
+    break
+}
--- tasks-class.js
+++ tasks-class.js
@@ -2,14 +2,23 @@ export class TasksList {
   constructor (defaultTasks = []) {
     this.tasks = defaultTasks
     this._id = 0
+    this._tasksStatus = ['done', 'not done', 'in-progress']
   }
 
-  addTask ({ description }) {
-    this.tasks.push({ title: description, id: this.tasks.length })
+  showTasks (tasks) {
+    console.log(`${'ID'.toString().padEnd(5)} ${'Task'.padEnd(15)} ${'Date'.padEnd(24)} ${'Status'.padStart(10)}`)
+    tasks.forEach(({ id, task, date, status }) => {
+      console.log(`${id.toString().padEnd(5)} ${task.padEnd(15)} ${date.padEnd(24)} ${status.padStart(10)}`)
+    })
   }
 
-  deleteTask (id) {
-    const newTasks = this.tasks.filter(values => values.id !== id)
-    this.tasks = newTasks
+  list ({ status }) {
+    if (this._tasksStatus.some(value => value === status)) {
+      const filterdTasks = this.tasks.filter(value => value.status === status)
+      this.showTasks(filterdTasks)
+    } else {
+      console.log('There is no status like this\nShowing all tasks')
+      this.showTasks(this.tasks)
+    }
   }
 }
